3829650edef28d5098d016203b1e4d26109bff20
Tiago José Pires Oliveira <tiago.oliveira500@gmail.com>
2021-02-11 12:09:37 +0000

    uno-picture add missing imports(UNO-137); uno-datepicker2 remove unnecessary html code; update uno-components version 0.0.118

--- projects/uno-ui-lib/src/lib/picture/picture.module.ts
+++ projects/uno-ui-lib/src/lib/picture/picture.module.ts
@@ -3,11 +3,13 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { UnoIconModule } from '../icon/icon.module';
 import { UnoModalModule } from '../modal/modal.module';
+import { UnoButtonModule } from '../button/button.module';
+import { UnoImageCropperModule } from './../image-cropper/image-cropper.module';
 
 import { PictureComponent } from './picture.component';
 
 @NgModule({
-    imports: [CommonModule, TranslateModule, UnoIconModule, UnoModalModule],
+    imports: [CommonModule, TranslateModule, UnoIconModule, UnoModalModule, UnoButtonModule, UnoImageCropperModule],
     exports: [PictureComponent],
     declarations: [PictureComponent],
     providers: [],
